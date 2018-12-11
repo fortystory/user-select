a5f5f6bc9f14d825cccca3ebeb9447b5829885fa
修伟 <forty40@live.cn>
2018-12-11 10:32:46 +0800

    keep old body style

--- user_select.js
+++ user_select.js
@@ -1,3 +1,4 @@
 window.document.onselectstart = () => true;
 window.document.oncopy = () => true;
-window.document.body.setAttribute('style', 'user-select: auto; -webkit-user-select: auto; -moz-user-select: auto;');
+var _old_style = window.document.body.getAttribute('style');
+window.document.body.setAttribute('style', 'user-select: auto; -webkit-user-select: auto; -moz-user-select: auto;'+_old_style);
