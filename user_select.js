cee7874c83c974f7c39b6af477b6244c4e98ef7c
修伟 <forty40@live.cn>
2019-03-04 17:34:56 +0800

    fix auto失败的情况

--- user_select.js
+++ user_select.js
@@ -1,4 +1,4 @@
 window.document.onselectstart = () => true;
 window.document.oncopy = () => true;
 var _old_style = window.document.body.getAttribute('style');
-window.document.body.setAttribute('style', 'user-select: auto; -webkit-user-select: auto; -moz-user-select: auto;'+_old_style);
+window.document.body.setAttribute('style', 'user-select: text; -webkit-user-select: text; -moz-user-select: text;'+_old_style);
