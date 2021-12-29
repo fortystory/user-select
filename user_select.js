08160d154349d56bd6f7717481c354cd35756c1f
fortystory <forty40@live.cn>
2021-12-30 01:24:27 +0800

    fix selectstart event

--- user_select.js
+++ user_select.js
@@ -2,3 +2,5 @@ window.document.onselectstart = () => true;
 window.document.oncopy = () => true;
 var _old_style = window.document.body.getAttribute('style');
 window.document.body.setAttribute('style', 'user-select: text; -webkit-user-select: text; -moz-user-select: text;'+_old_style);
+
+document.querySelector("body").onselectstart = () => {return true};
